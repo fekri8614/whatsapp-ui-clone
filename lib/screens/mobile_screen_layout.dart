56f2a210527f20e7f6aa6616a34fa97a5d9b3291
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-27 07:52:07 -0700

    completed ui of app

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
@@ -11,6 +12,7 @@ class MobileScreenLayout extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: appBarColor,
+          elevation: 0,
           title: const Text(
             'WhatsApp',
             style: TextStyle(
@@ -51,10 +53,7 @@ class MobileScreenLayout extends StatelessWidget {
             ],
           ),
         ),
-        body: Text(
-          'Hello, world :-)',
-          style: TextStyle(color: Colors.white),
-        ),
+        body: ContactsList(),
       ),
     );
   }
